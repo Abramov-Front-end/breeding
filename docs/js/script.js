79795b983a884ec404e2689167d0424d9edd8824
Yevhen Abramov <abramovyevhen@MacBook-Pro-Yevhen.local>
2022-07-10 07:23:05 +0300

    Slide down

--- docs/js/script.js
+++ docs/js/script.js
@@ -3,10 +3,15 @@ const revealNow = document.getElementById('revealNow')
 const cryogenicEl = document.querySelector('.cryogenic')
 let creoCamera = document.getElementById('creoCamera')
 let potionsSlider = document.getElementById('potionsSlider')
+let myBreedingSlider = document.getElementById('myBreedingSlider')
 
-const breedingState = {
+let breedingState = {
     status: false,
-    step: 'start'
+    step: 'start',
+    potion: null,
+    parents: [],
+    babyid: null,
+    datestart: null
 }
 
 //Wallet
@@ -33,10 +38,10 @@ walletBlock.addEventListener('click', function(e){
 })
 
 //Timers
-function Timer (el, timeStart, timeNow) {
+function Timer (el, timeStart) {
     this.el = el
-    this.timeStart = timeStart
-    this.timeNow = timeNow
+    this.timeStart = timeStart ? new Date(+timeStart) : new Date()
+
     this.interval = null
 }
 Timer.prototype.start = function (stopFunc) {
@@ -46,14 +51,27 @@ Timer.prototype.start = function (stopFunc) {
     this.stopFunc = stopFunc
 
     this.interval = setInterval(function(){
-        const timeLeft = timeFinished - new Date().getTime()
+        const dateNow = new Date().getTime()
+        const timeLeft = timeFinished - dateNow
 
         if ( timeLeft > 0 ) {
-            const minutes = Math.floor(timeLeft / (1000 * 60));
-            const seconds = Math.floor(timeLeft / 1000);
-            this.el.innerHTML = (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds);
+            let s = Math.floor(timeLeft / 1000);
+            let m = Math.floor(s / 60);
+
+            s %= 60;
+            m %= 60;
+
+            s = (s < 10) ? "0" + s : s;
+            m = (m < 10) ? "0" + m : m;
+
+            this.el.innerHTML = m + ':' + s;
+
+            if(this.el.closest('.nft-item')) {
+                let progress = (timeFinished - this.timeStart.getTime() - timeLeft) / (timeFinished - this.timeStart.getTime()) * 100
+                this.el.parentElement.querySelector('.nft-item__progress-bar__handler').style.width = Math.floor(progress) + '%'
+            }
         } else {
-            this.el.innerHTML = '00:00'
+            this.el.innerHTML = ':'
             this.stop(this.stopFunc)
         }
 
@@ -96,7 +114,15 @@ Breeding.prototype.start = function() {
 
     if ( !this.checkReady() ) return false
 
+    const potionTitle = this.cryogenicEl.querySelector('.potion .potion__title b')
+    potionTitle.innerHTML = '#' + breedingState.potion
+
+    const babyid = Math.floor(+breedingState.parents[0]+breedingState.parents[0] + Math.floor(Math.random()*1000))
+
     breedingState.step = 'in-progress'
+    breedingState.datestart = new Date().getTime()
+
+    sliders.nft.appendSlide(babyid)
 
     this.cryogenicEl.className = 'cryogenic started step_1'
     this.sounds.water.play()
@@ -106,7 +132,7 @@ Breeding.prototype.start = function() {
 
     const timerEl = creoCamera.querySelector('.breeding-timer')
 
-    this.timer = new Timer(timerEl, new Date(), new Date())
+    this.timer = new Timer(timerEl)
     this.timer.start(this.finish.bind(this))
 
     this.timeoutFunc(function() {
@@ -179,9 +205,8 @@ Breeding.prototype.reload = function() {
     breedingState.step = 'start'
     breeding.checkReady()
 }
-Breeding.prototype.finish = function() {
-
-    this.cryogenicEl.className = 'cryogenic in-progress completed'
+Breeding.prototype.finish = function(opened) {
+    this.cryogenicEl.className = 'cryogenic in-progress completed' + (opened ? ' opened' : '')
     this.sounds.complete.play()
     this.sounds.robot.pause()
 
@@ -190,10 +215,34 @@ Breeding.prototype.finish = function() {
     }.bind(this), 100)
 
     setTimeout(function(){
-        this.cryogenicEl.className = 'cryogenic completed on'
+        this.cryogenicEl.className = 'cryogenic completed ' + (opened ? 'opened' : 'on')
     }.bind(this), 1100)
 
     breedingState.step = 'completed'
+}
+Breeding.prototype.open = function(data) {
+
+    breedingState.status = true
+    breedingState = {...breedingState, ...data}
+    breedingState.parents = data.parents.split(',')
+
+    sliders.cryo.goToSlideById(breedingState.parents)
+    sliders.cryo.disable()
+
+    this.cryogenicEl.className = 'cryogenic ' + breedingState.step  + ' opened'
+
+    if ( breedingState.step === 'in-progress' ) {
+        this.sounds.robot.play()
+    } else {
+        this.sounds.complete.play()
+    }
+
+    const potionTitle = this.cryogenicEl.querySelector('.potion .potion__title b')
+    potionTitle.innerHTML = '#' + breedingState.potion
+
+    const timerEl = creoCamera.querySelector('.breeding-timer')
+    this.timer = new Timer(timerEl, breedingState.datestart)
+    this.timer.start(this.finish.bind(this, true))
 }
 Breeding.prototype.checkReady = function() {
 
@@ -265,25 +314,63 @@ const sliders = {
                         },
                         on: {
                             init: function(swiper) {
-                                const swiperOverflow = swiper.$el.closest('.swiper-overflow')[0]
-                                if ( swiperOverflow ) {
-                                    if ( !swiper.slides.length ) {
-                                        swiperOverflow.classList.add('empty')
-                                    } else {
-                                        swiperOverflow.classList.remove('empty')
-                                    }
-                                }
+                                sliders.nft.checkEmpty(swiper)
                             }
                         }
                     })
                 )
+            })
+        },
+        checkEmpty: function(swiper) {
+            const swiperOverflow = swiper.$el.closest('.swiper-overflow')[0]
+            if ( swiperOverflow ) {
+                if ( !swiper.slides.length ) {
+                    swiperOverflow.classList.add('empty')
+                } else {
+                    swiperOverflow.classList.remove('empty')
+                }
+            }
+        },
+        appendSlide: function(babyid) {
+            breedingState.babyid = babyid
+            this.array[1].prependSlide([
+                `<div class="swiper-slide nft-item" 
+                    data-babyid="${babyid}" 
+                    data-potion="${breedingState.potion}"
+                    data-parents="${breedingState.parents}"
+                    data-datestart="${breedingState.datestart}"
+                    data-step="in-progress">
+                    <div class="nft-item__indicator nft-item__timer">:</div>
+                    <img src="images/new-nft.svg" alt="" class="nft-item__img">
+                    <div class="nft-item__progress-bar">
+                        <div class="nft-item__progress-bar__handler" style="width: 0%"></div>
+                    </div>
+                </div>`
+            ])
+
+            sliders.nft.checkEmpty(this.array[1])
+            sliders.nft.slideTimer(babyid)
+        },
+        slideTimer: function(babyid) {
+            const slide = this.array[1].el.querySelector('[data-babyid="' + babyid + '"]')
+            const timerEl = slide.querySelector('.nft-item__timer')
+            const timer = new Timer(timerEl, new Date())
+            timer.start(function(){
+                const revealButton = document.createElement('div')
+                revealButton.className = 'nft-item__indicator nft-item__repack'
+                revealButton.innerHTML = 'Reveal Now!'
+                revealButton.dataset.id = babyid
+                timerEl.remove()
+                slide.prepend(revealButton)
+                slide.querySelector('.nft-item__progress-bar').classList.add('finished')
+                slide.dataset.step = 'completed'
             })
         }
     },
     cryo: {
         array: [],
         init: function(el) {
-            el.forEach(e => {
+            el.forEach((e, i) => {
                 this.array.push(
                     new Swiper(e, {
                         effect: 'coverflow',
@@ -309,6 +396,7 @@ const sliders = {
                         on: {
                             slideChange: function() {
                                 breeding.checkReady()
+                                breedingState.parents[i] = e.swiper.slides[e.swiper.activeIndex].dataset.id
                             }
                         }
                     })
@@ -349,13 +437,22 @@ const sliders = {
                 }
             }
             return status
+        },
+        goToSlideById: function(parents) {
+            this.array.forEach(function(e, i) {
+                for ( let slide in e.slides ) {
+                    if ( e.slides[slide].dataset.id === parents[i] ) {
+                        e.slideTo(slide)
+                        break
+                    }
+                }
+            })
         }
     }
 }
 sliders.scroll.init()
 sliders.nft.init(document.querySelectorAll('.swiper'))
 sliders.cryo.init(document.querySelectorAll('.cryo-swiper'))
-
 
 //EventListeners
 cryogenicEl.addEventListener('click', function(e) {
@@ -369,10 +466,14 @@ potionsSlider.addEventListener('click', function(e) {
     if ( potion && !potion.classList.contains('locked') && breedingState.step !== 'potion-checked') {
         potion.classList.add('locked')
         breedingState.step = 'potion-checked'
-
+        breedingState.potion = potion.dataset.id
         breeding.checkReady()
     }
+})
+myBreedingSlider.addEventListener('click', function(e) {
+    const myBreeding = e.target.closest('.swiper-slide')
 
+    breeding.open(myBreeding.dataset)
 })
 breedingNew.addEventListener('click', function(e){
     e.preventDefault()
