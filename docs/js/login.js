44e79eeed62c1fe12e19e1fc293df5f1073c12a5
Yevhen Abramov <abramovyevhen@MacBook-Pro-Yevhen.local>
2022-07-19 21:31:20 +0300

    Login page

---
+++ docs/js/login.js
@@ -0,0 +1,56 @@
+const loginArea = document.querySelector('.login-area')
+const loginBtn = document.getElementById('loginBtn')
+const logoutBtn = document.getElementById('logoutBtn')
+
+function Auth() {
+    this.step = 0
+    this.area = loginArea
+}
+
+Auth.prototype.switchStep = function(duration) {
+
+    if ( duration === 'next' ) {
+        this.step++
+    } else {
+        this.step--
+    }
+    console.log(this.step)
+
+    const steps = this.area.querySelectorAll('[class^="step_"]')
+
+    steps.forEach(function(e){
+        e.classList.remove('show')
+    })
+    this.area.querySelector(`.step_${this.step}`).classList.add('show')
+}
+
+Auth.prototype.login = function() {
+    this.switchStep('next')
+}
+
+Auth.prototype.logout = function() {
+    this.switchStep('prev')
+}
+
+const auth = new Auth()
+
+loginBtn.addEventListener('click', function(e) {
+    e.preventDefault()
+    auth.login()
+})
+logoutBtn.addEventListener('click', function(e) {
+    e.preventDefault()
+    auth.logout()
+})
+loginArea.addEventListener('click', function(e) {
+
+    if ( e.target.classList.contains('controls-buttons-next') ) {
+        e.preventDefault()
+        auth.switchStep('next')
+    }
+    if ( e.target.classList.contains('controls-buttons-prev') ) {
+        e.preventDefault()
+        auth.switchStep('prev')
+    }
+
+})
